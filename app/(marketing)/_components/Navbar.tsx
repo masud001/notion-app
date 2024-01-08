f4148c8eff8d0021acc12c32f537934942263fce
masud001 <madhnagar@gmail.com>
2024-01-08 22:31:58 +0600

    landing page finish

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,18 +4,19 @@
 import useScrollTop from '@/hooks/use-scroll-top';
 import { cn } from '@/lib/utils';
 import Logo from './Logo';
+import { ModeToggle } from '@/components/mode-toggle';
 
 const Navbar = () => {
 	const scrolled = useScrollTop();
 	return (
 		<div
 			className={cn(
-				'z-50 bg-background fixed top-0 p-6 w-full flex items-center',
+				'z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 p-6 w-full flex items-center',
 				scrolled && 'border-b shadow-sm'
 			)}>
 			<Logo />
 			<div className='md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2'>
-				login
+				<ModeToggle />
 			</div>
 		</div>
 	);
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -6,7 +6,7 @@ import Heroes from './_components/Heroes';
 
 export default function Home() {
 	return (
-		<div className='min-h-full flex flex-col'>
+		<div className='min-h-full bg-background dark:bg-[#1F1F1F] flex flex-col'>
 			<div className='flex flex-col items-center justify-center text-center md:justify-start  gap-y-8 flex-1 px-6 bp-10'>
 				<Heading />
 				<Heroes />
